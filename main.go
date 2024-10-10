15364414caec9eb4be897ed12c8e8bb5c8009186
Mike Joyce <mike.joyce@shipt.com>
2024-10-10 08:51:54 -0400

    download images

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 type PokemonResponse struct {
@@ -35,12 +37,27 @@ const ALL_POKEMON_URL = "https://pokeapi.co/api/v2/pokemon?limit=400"
 const POKEMON_DETAIL_URL = "https://pokeapi.co/api/v2/pokemon/%s"
 
 func main() {
-	resp, err := http.Get(fmt.Sprintf(POKEMON_DETAIL_URL, "pikachu"))
+	pokemonName := "snorlax"
+	resp, err := http.Get(fmt.Sprintf(POKEMON_DETAIL_URL, pokemonName))
 	panicIfErr(err)
 
+	defer resp.Body.Close()
 	var pokemonResponse PokemonResponse
 	dec := json.NewDecoder(resp.Body)
 	dec.Decode(&pokemonResponse)
 
-	fmt.Printf("Name: %s, Id: %d, Sprite: %s", pokemonResponse.Name, pokemonResponse.Id, pokemonResponse.Sprites.Default)
+	resp, err = http.Get(pokemonResponse.Sprites.Default)
+	panicIfErr(err)
+	defer resp.Body.Close()
+
+	// read contents of image response
+	fileName := fmt.Sprintf("./images/%s.jpg", pokemonName)
+	file, fileErr := os.Create(fileName)
+	panicIfErr(fileErr)
+
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	panicIfErr(err)
+	fmt.Printf("image saved to %s", fileName)
 }
