63c00696ab335265a3fced3570682735d1eaf6a1
Mike Joyce <mike.joyce@shipt.com>
2024-10-10 08:31:14 -0400

    add more types, split up urls

--- main.go
+++ main.go
@@ -16,14 +16,26 @@ type PokemonSpritesResponse struct {
 	Default string `json:"front_default"`
 }
 
+type AllPokemonsResponse struct {
+	Results []AllPokemonResponse `json:"results"`
+}
+
+type AllPokemonResponse struct {
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
+
 func panicIfErr(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
+const ALL_POKEMON_URL = "https://pokeapi.co/api/v2/pokemon?limit=400"
+const POKEMON_DETAIL_URL = "https://pokeapi.co/api/v2/pokemon/%s"
+
 func main() {
-	resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/snorlax")
+	resp, err := http.Get(fmt.Sprintf(POKEMON_DETAIL_URL, "pikachu"))
 	panicIfErr(err)
 
 	var pokemonResponse PokemonResponse
