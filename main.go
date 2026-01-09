e433348a3f853873b0808d12c8a10e1cb9c36d48
Mike Joyce <mike.joyce@shipt.com>
2026-01-09 15:56:32 -0500

    cleanup types and enums

--- main.go
+++ main.go
@@ -6,30 +6,6 @@ import (
 	"mcmickjuice/pokego/pokemon"
 )
 
-type PokemonResponse struct {
-	Name    string                 `json:"name"`
-	Id      int                    `json:"id"`
-	Sprites PokemonSpritesResponse `json:"sprites"`
-}
-
-type PokemonSpritesResponse struct {
-	Default     string `json:"front_default"`
-	BackDefault string `json:"back_default"`
-	FrontShiny  string `json:"front_shiny"`
-}
-
-type AllPokemonsResponse struct {
-	Results []AllPokemonResponse `json:"results"`
-}
-
-type AllPokemonResponse struct {
-	Name string `json:"name"`
-	Url  string `json:"url"`
-}
-
-const ALL_POKEMON_URL = "https://pokeapi.co/api/v2/pokemon?limit=400"
-const POKEMON_DETAIL_URL = "https://pokeapi.co/api/v2/pokemon/%s"
-
 func main() {
 	pokemonPtr := flag.String("pokemon", "snorlax", "give a pokemon")
 	flag.Parse()
