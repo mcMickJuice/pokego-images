7eea9bace069c3670a54ab24dba68c9c8d65fefe
Mike Joyce <mikejoyce19@gmail.com>
2026-01-12 06:14:35 -0500

    dont panic

--- internal/webserver/handler.go
+++ internal/webserver/handler.go
@@ -2,27 +2,37 @@ package webserver
 
 import (
 	"fmt"
+	"mcmickjuice/pokego/internal/pokeimage"
+	"mcmickjuice/pokego/internal/pokemon"
 	"net/http"
 )
 
 func CreateWebserver() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/pokemon/{pokemon}", func(w http.ResponseWriter, r *http.Request) {
-
 		param := r.PathValue("pokemon")
 		fmt.Printf("pokemon param: %s", param)
 
-		response := "hello"
-		_, err := w.Write([]byte(response))
-		if err != nil {
+		pokemonClient := pokemon.NewPokemonClient(param)
+		image, err := pokemonClient.GetPokemonSprite()
 
+		if err != nil {
+			// unwrap to see if not found or not? maybe return sentinal error here
+			w.WriteHeader(http.StatusNotFound)
+			_, _ = w.Write([]byte("Bad request"))
+			return
+		}
+
+		pokeimage.NewPokemonImage(image).PrintAsciiArt()
+		response := "hello"
+		_, err = w.Write([]byte(response))
+		if err != nil {
 			fmt.Printf("error writing response: %v", err)
 		}
 	})
 
 	err := http.ListenAndServe("localhost:8080", mux)
 	if err != nil {
-
 		return err
 	}
 
