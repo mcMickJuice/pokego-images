805486add23b94f497dca980423423c49ab28c7f
Mike Joyce <mikejoyce19@gmail.com>
2026-02-11 06:50:04 -0500

    write test

---
+++ internal/pokemon/client_test.go
@@ -0,0 +1,32 @@
+package pokemon
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_Client(t *testing.T) {
+
+	t.Run("get pokemon - not found", func(t *testing.T) {
+		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Printf("request for %s", r.URL)
+			w.WriteHeader(http.StatusNotFound)
+		}))
+
+		client := NewPokemonClient(s.URL)
+
+		_, err := client.GetPokemon(context.Background(), "pikachu")
+
+		if err == nil {
+
+			t.Fatal("expected error, got none")
+		}
+		if !errors.Is(err, ErrPokemonNotFound) {
+			t.Fatalf("expected not found error, got different error %v", err)
+		}
+	})
+}
