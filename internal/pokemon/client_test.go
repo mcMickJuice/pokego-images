1abf6c3583a7ce10cb8752cfa7d47629dd25cc24
Mike Joyce <mikejoyce19@gmail.com>
2026-02-11 07:59:14 -0500

    write another test

--- internal/pokemon/client_test.go
+++ internal/pokemon/client_test.go
@@ -11,6 +11,24 @@ import (
 
 func Test_Client(t *testing.T) {
 
+	t.Run("get pokemon - internal server error", func(t *testing.T) {
+		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}))
+
+		client := NewPokemonClient(s.URL)
+
+		_, err := client.GetPokemon(context.Background(), "pikachu")
+
+		if err == nil {
+			t.Fatal("expected error, got none")
+		}
+
+		if errors.Is(err, ErrPokemonNotFound) {
+			t.Fatal("expected generic error, got not found error")
+		}
+
+	})
 	t.Run("get pokemon - not found", func(t *testing.T) {
 		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("request for %s", r.URL)
